207bc368bb601f9d4e6dfdbb43ec0d5fdb5c917f
ennyitan <ololadedamilola7@gmail.com>
2023-02-05 22:53:44 +0100

    update

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { AccomplishmentCardsComponent } from './accomplishment-cards/accomplishment-cards.component';
 import { FocalSectorsCardsComponent } from './focal-sectors-cards/focal-sectors-cards.component';
+import { MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { FocalSectorsCardsComponent } from './focal-sectors-cards/focal-sectors-
     AccomplishmentCardsComponent,
     FocalSectorsCardsComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatMenuModule],
   exports: [
     HeaderComponent,
     FooterComponent,
