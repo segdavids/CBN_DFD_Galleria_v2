6028f2edb8cec1f359fecee07ab03f390b990702
HyOcee <eosemegbe@gmail.com>
2023-02-06 00:28:36 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductsComponent } from './pages/landing/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/landing/landing.module.ts
+++ src/app/pages/landing/landing.module.ts
@@ -14,6 +14,7 @@ import { LandingLayoutComponent } from './landing-layout/landing-layout.componen
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatExpansionModule} from '@angular/material/expansion';
 import { NgChartsModule } from 'ng2-charts';
+import { ProductsComponent } from './products/products.component';
 
 
 
@@ -27,7 +28,8 @@ import { NgChartsModule } from 'ng2-charts';
     HealthComponent,
     InfrastructureComponent,
     ExportComponent,
-    LandingLayoutComponent
+    LandingLayoutComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,
