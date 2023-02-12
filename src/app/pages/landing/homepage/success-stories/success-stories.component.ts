3f50c8759ce925759f37e4cd2482255a6f0dba46
HyOcee <eosemegbe@gmail.com>
2023-02-12 21:09:15 +0100

    update

--- src/app/pages/landing/homepage/success-stories/success-stories.component.ts
+++ src/app/pages/landing/homepage/success-stories/success-stories.component.ts
@@ -21,11 +21,26 @@ export class SuccessStoriesComponent {
       0: {
         items: 1
       },
-      650: {
-        items: 3
+    },
+    nav: false,
+    navText: ["<img class='nav-button owl-prev'  src='assets/icons/left-arrow.svg'>", "<img class='nav-button owl-next'  src='assets/icons/right-arrow.svg'>"],
+  }
+  customOptions2: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    autoplay: true,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 2.5
       },
     },
-    nav: true,
+    nav: false,
+    // stagePadding: 500,
     navText: ["<img class='nav-button owl-prev'  src='assets/icons/left-arrow.svg'>", "<img class='nav-button owl-next'  src='assets/icons/right-arrow.svg'>"],
   }
 
