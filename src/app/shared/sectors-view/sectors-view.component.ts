4b4422ae46ab12c14dfb36a4d28b74abf5c6a7a7
HyOcee <eosemegbe@gmail.com>
2023-02-12 10:44:50 +0100

    update

--- src/app/shared/sectors-view/sectors-view.component.ts
+++ src/app/shared/sectors-view/sectors-view.component.ts
@@ -3,12 +3,21 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-sectors-view',
   templateUrl: './sectors-view.component.html',
-  styleUrls: ['./sectors-view.component.scss']
+  styleUrls: ['./sectors-view.component.scss'],
 })
 export class SectorsViewComponent {
-  @Input() header!: string
+  @Input() header!: string;
   @Input() headerBanner!: string;
-  @Input() stats!: { stat: string, text: string, image: string }[]
-  @Input() listing!: {image: string, title: string, shortTitle: string, list: string[]}[]
-
+  @Input() stats!: {
+    stat: string;
+    text: string;
+    image: string;
+    mt?: boolean;
+  }[];
+  @Input() listing!: {
+    image: string;
+    title: string;
+    shortTitle: string;
+    list: string[];
+  }[];
 }
